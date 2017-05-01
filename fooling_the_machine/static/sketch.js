bdfda6228286c437b168da65df8897048dd88ea3
Ada Zhao <adazhao1211@gmail.com>
2017-05-01 14:33:41 -0400

    canvas->img

--- fooling_the_machine/static/sketch.js
+++ fooling_the_machine/static/sketch.js
@@ -5,7 +5,6 @@
 // Submit button
 var submit;
 // Show results
-var resultP;
 var next = false;
 var w, h, wg, hg;
 var drawing = false;
@@ -15,8 +14,11 @@ var store = [];
 var tr = false;
 var finalimg, finalnumber;
 var count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var imgs, base64, slider;
 function preload(){
   font = loadFont('AdobeClean-Bold.otf');
+  imgs = $('body');
+
 }
 function setup() {
   textFont(font);
@@ -24,31 +26,33 @@ function setup() {
   h = windowHeight;
   wg = (w-1000)/6;
   hg = (h-800)/6;
-  var canvas = createCanvas(w, h);
-  resultP = createP(' ');
-  submit = createButton('classify');
+  slider = createSlider(5, 40, 30);
+  slider.position(w/2-150, 450+2*hg);
+  slider.style('width', '200px');
+  var canvas = createCanvas(w, 4*hg+400);
+  submit = createButton('submit');
+
   // When the button is pressed classify!
   submit.mousePressed(classify);
   background(0);
   stroke(210);
   strokeWeight(5);
   noFill();
-  rect(w/2-205, 2*hg-5, 410, 410);
+  rect(w/2-210, 2*hg-10, 420, 420);
   textSize(200);
+  stroke(255);
+  strokeWeight(5);
+  point(w/2-180, 450+2*hg);
+  strokeWeight(40);
+  point(w/2+80, 450+2*hg);
   for(var i = 0; i < 10; i++){
     store[i] = [];
   }
   for(var i = 0; i < 5; i++){
-    noStroke();
-    fill(210);
-    rect(wg+i*200+i*wg, 400+4*hg, 200, 200);
     if(i == 1) store[i] = font.textToPoints(i.toString(), wg+i*200+i*wg+60, 570+4*hg);
     else store[i] = font.textToPoints(i.toString(), wg+i*200+i*wg+50, 570+4*hg);
   }
   for(var i = 0; i < 5; i++){
-    noStroke();
-    fill(210);
-    rect(wg+i*200+i*wg, 600+5*hg, 200, 200);
     if(i == 2) store[i+5] = font.textToPoints((i+5).toString(), wg+i*200+i*wg+60, 770+5*hg);
     else store[i+5] = font.textToPoints((i+5).toString(), wg+i*200+i*wg+50, 770+5*hg);
   }
@@ -67,7 +71,7 @@ function draw() {
     if(mouseX<w/2+200 && mouseX>w/2-200 && mouseY>hg*2 && mouseY < hg*2+400){
       if(drawing){
         stroke(255);
-        strokeWeight(15);
+        strokeWeight(slider.value());
         line(pmouseX, pmouseY, mouseX, mouseY);
       }
       drawing = true;
@@ -77,9 +81,9 @@ function draw() {
   }
   if(next){
     stroke(210);
-    strokeWeight(5);
+    strokeWeight(10);
     fill(0);
-    rect(w/2-205, 2*hg-5, 410, 410);
+    rect(w/2-210, 2*hg-10, 420, 420);
     next = false;
   }
 }
@@ -89,7 +93,7 @@ function classify() {
   // Get all the pixels!
   finalimg = get(w/2-200, hg*2, 400, 400);
   // Turn it to a base64 encoded string
-  var base64 = finalimg.canvas.toDataURL();
+  base64 = finalimg.canvas.toDataURL();
   // Get rid of the header stuff (should maybe do this in flask)
   var cleaned = base64.replace('data:image/png;base64,', '');
   // Make an object to post
@@ -111,24 +115,25 @@ function classify() {
       // Look at number and probability (confidence)
       finalnumber = result.number;
       var confidence = result.prediction[finalnumber];
-      // Display the results in the paragraph element
-      resultP.html(finalnumber + '<br/>' + 'confidence: ' + nf(100 * confidence, 2, 1) + '%');
-    } else {
-      resultP.html('error');
+
     }
-    trans(finalimg, finalnumber);
+    next = true;
+    if(count[finalnumber]+1 == store[finalnumber].length){
+      count[finalnumber] = 0;
+    }
+    else count[finalnumber]+=1;
+    var c = count[finalnumber];
+    var i = new Image();
+    i.src=base64;
+    i.style.position = "absolute";
+    i.width = 20;
+    i.height = 20;
+    i.style.left = store[finalnumber][c].x;
+    i.style.top = store[finalnumber][c].y;
+    imgs.append(i).fadeIn();
   }
 
   function error(reply) {
-    resultP.html('error');
+    console.log('error');
   }
-}
-function trans(img, num){
-  next = true;
-  if(count[num]+1 == store[num].length){
-    count[num] = 0;
-  }
-  else count[num]+=1;
-  var c = count[num];
-  image(img, store[num][c].x, store[num][c].y, 10, 10);
 }
